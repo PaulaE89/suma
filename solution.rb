83775c8481d8245ae1ecae5264568a18dbef57b7
Paula <43395223+PaulaE89@users.noreply.github.com>
2019-05-15 21:51:49 -0500

    suma

---
+++ solution.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+get '/' do
+@count=params[:item]
+@count=0 if @count==nil
+erb :solution
+end
+
+post'/count' do
+
+@count= (params[:c].to_i) + 1
+
+redirect "/?item=#{@count}"
+
+end
